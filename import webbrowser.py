f4593fe511828e3d4a2e063bebae7ed30d175c57
SNot143 <shanekianm@gmail.com>
2025-06-25 18:27:25 +0800

    Add files via upload

---
+++ import webbrowser.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+# Google Drive URL to your virtual resume
+drive_url = "https://drive.google.com/file/d/17U98rhB7SlnHx1H-wA3HBXjRu9gvyBnf/view?usp=sharing"
+
+# Open the URL in the default web browser
+webbrowser.open(drive_url)
+
+print("Opening virtual resume in browser...")
